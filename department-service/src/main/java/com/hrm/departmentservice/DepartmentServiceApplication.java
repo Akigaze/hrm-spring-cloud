0d982eb48de79c8f1d2546b889c1399e97ce5703
Quinn <quinn.huang@oocl.com>
2019-04-01 20:33:39 +0800

    add hystrix dashboard module

--- department-service/src/main/java/com/hrm/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/hrm/departmentservice/DepartmentServiceApplication.java
@@ -2,11 +2,12 @@ package com.hrm.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringCloudApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
